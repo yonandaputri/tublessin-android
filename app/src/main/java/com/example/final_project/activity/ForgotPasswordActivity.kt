10a4747df1632e7dd87ef229f389c720ab1ad19e
yonandaputri <yonandaputri12@gmail.com>
2020-09-05 23:17:11 +0700

    first commit

---
+++ app/src/main/java/com/example/final_project/activity/ForgotPasswordActivity.kt
@@ -0,0 +1,13 @@
+package com.example.final_project.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.final_project.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+    }
+}
